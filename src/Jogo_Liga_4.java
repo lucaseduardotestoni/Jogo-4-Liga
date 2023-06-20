b440a1e6049e2a8ee30e4a75a5d03c8496089a93
lucaseduardotestoni <testonilucas015@gmail.com>
2023-06-20 10:02:44 -0300

    Lucas Testoni: Atualização Final

--- src/Jogo_Liga_4.java
+++ src/Jogo_Liga_4.java
@@ -4,65 +4,69 @@ public class Jogo_Liga_4 {
     private Jogo_Liga_4() {
         Scanner teclado = new Scanner(System.in);
         char tabuleiro[][] = new char[6][7];
-        char jogador = ' ';
-        char computador = ' ';
+        char computador;
+        char jogador;
+        char op;
         boolean verifica;
-        System.out.println("================Jogo Liga 4 Iniciado=================");
         do {
+            jogador = ' ';
+            computador = ' ';
             verifica = true;
-            System.out.println("jogador deve escolher a sua cor entre V (vermelho) ou A (azul)");
-            jogador = teclado.next().charAt(0);
-            jogador = Character.toUpperCase(jogador);
-            if (jogador == 'V') {
-                computador = 'A';
-            } else if (jogador == 'A') {
-                computador = 'V';
-            } else {
-                System.out.println("Letra Informada Incorreta");
-                verifica = false;
-            }
-        } while (!verifica);
-        jogar(tabuleiro, teclado, jogador, computador);
+            System.out.println("================Jogo Liga 4 Iniciado=================");
+            do {
+                verifica = true;
+                System.out.println("jogador deve escolher a sua cor entre V (vermelho) ou A (azul)");
+                jogador = teclado.next().charAt(0);
+                jogador = Character.toUpperCase(jogador);
+                if (jogador == 'V') {
+                    computador = 'A';
+                } else if (jogador == 'A') {
+                    computador = 'V';
+                } else {
+                    System.out.println("Letra Informada Incorreta");
+                    verifica = false;
+                }
+            } while (!verifica);
+            jogar(tabuleiro, teclado, jogador, computador);
 
+            System.out.println("Deseja Jogar Novamente? (S)Sim ou (N)Não");
+            op = teclado.next().charAt(0);
+            op = Character.toUpperCase(op);
 
+        } while (op != 'N');
+
+        teclado.close();
     }
 
-    //Controle de todo o Jogo
+    //Controle do Jogo
     private static void jogar(char tabuleiro[][], Scanner teclado, char jogador, char computador) {
-        char op;
-        int jogadas;
+        int jogadas = 0;
         boolean ganhou = false;
 
-        do {
-            jogadas = 0;
-            op = ' ';
-            tabuleiroLimpar(tabuleiro);
+        tabuleiroLimpar(tabuleiro);
 
-            var startPlayer = sortear(-1, 2);
+        var startPlayer = sortear(-1, 2);
 
-            while (jogadas != 42) {
-                jogadas++;
-                if (startPlayer == 1 && jogadas <= 42) {
-                    ganhou = jogador(teclado, tabuleiro, jogador);
-                    startPlayer = 0;
+        while (jogadas < 42) {
+            jogadas++;
+            if (startPlayer == 1 && jogadas <= 42) {
+                ganhou = jogador(teclado, tabuleiro, jogador);
+                startPlayer = 0;
 
-                } else if (startPlayer == 0 && jogadas <= 42) {
-                    ganhou = computador(tabuleiro, computador);
-                    startPlayer = 1;
-                }
-                if (ganhou) {
-                    break;
-                }
-                desenhaTabuleiro(tabuleiro, jogadas);
+            } else if (startPlayer == 0 && jogadas <= 42) {
+                ganhou = computador(tabuleiro, computador);
+                startPlayer = 1;
+            }
+            desenhaTabuleiro(tabuleiro, jogadas);
+            if (ganhou) {
+                break;
             }
 
-            if (jogadas == 42 && ganhou == false) {
-                System.out.println("Empate!");
-                System.out.println("Deseja Jogar Novamente? (S)Sim ou (N)Não");
-                op = teclado.next().charAt(0);
-                op = Character.toUpperCase(op);
-            }
-        } while (op != 'N');
+        }
+        if (!ganhou) {
+            System.out.println("Empate!");
+        }
+
     }
 
     //Controle Jogadas Jogador
@@ -74,11 +78,11 @@ public class Jogo_Liga_4 {
 
         do {
             System.out.println("Insira coluna da jogada");
-            coluna = teclado.nextInt();//sortear(-1,5);
+            coluna = teclado.nextInt();
+
             if (coluna > 6 && coluna < 0) {
                 System.out.println("Valor Invalído, Informe Novamente");
-            }
-            if (coluna <= 6 && coluna >= 0) {
+            } else if (coluna <= 6 && coluna >= 0) {
                 linha = encontrarUltimaPosicaoLivre(tabuleiro, coluna);
             }
             if (linha == -1) {
@@ -102,7 +106,7 @@ public class Jogo_Liga_4 {
         int linha;
         System.out.println("Computador Jogando..");
         do {
-            int coluna = sortear(-1, 6);
+            int coluna = sortear(-1, 7);
             linha = encontrarUltimaPosicaoLivre(tabuleiro, coluna);
             if (linha != -1) {
                 verifica = true;
@@ -117,13 +121,27 @@ public class Jogo_Liga_4 {
         return ganhou;
     }
 
+    //Cores por Unicode
+    public static String setarCores(int Ncor){
+        String cor = " ";
+
+     switch (Ncor){
+         case 1://Vermelho
+             return "\033[0;31m";
+         case 2://Azul
+             return  "\033[0;34m";
+             //Padrão
+         default:  return "\033[0m";
+     }
+
+    }
     //Desenha o tabuleiro
     public static void desenhaTabuleiro(char tabuleiro[][], int jogada) {
         //Barra Superior
         for (int j = 0; j < tabuleiro.length; j++) {
             System.out.print("----");
         }
-        System.out.println("----. Jogada " + (jogada + 1));
+        System.out.println("----. Jogada " + (jogada));
         System.out.print(" ");
         for (int j = 0; j < tabuleiro.length + 1; j++) {
             System.out.print("| " + j + " ");
@@ -133,7 +151,14 @@ public class Jogo_Liga_4 {
         for (int coluna = 0; coluna < tabuleiro.length; coluna++) {
             System.out.print(coluna);
             for (int linha = 0; linha < (tabuleiro.length + 1); linha++) {
-                System.out.print("| " + tabuleiro[coluna][linha] + " ");
+                if (tabuleiro[coluna][linha] == 'A') {
+                    System.out.print("| " + setarCores(2) + tabuleiro[coluna][linha] + setarCores(3)  + " ");
+                } else if (tabuleiro[coluna][linha] == 'V') {
+                    System.out.print("| " + setarCores(1) + tabuleiro[coluna][linha] + setarCores(3) + " ");
+                } else {
+                    System.out.print("| " + setarCores(3) + tabuleiro[coluna][linha] + " ");
+                }
+
             }
             //Separa os Elementos um dos Outros
             System.out.println("|");
@@ -148,7 +173,7 @@ public class Jogo_Liga_4 {
 
     //Define Valor B para todos os Campos da matriz do Tabuleiro
     public static void tabuleiroLimpar(char tabuleiro[][]) {
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro.length + 1; j++) {
                 tabuleiro[i][j] = 'B';
             }
@@ -183,6 +208,40 @@ public class Jogo_Liga_4 {
                     break;
                 } else if (tabuleiro[i][j] == tabuleiro[i][j + 1] && tabuleiro[i][j] == tabuleiro[i][j + 2] && tabuleiro[i][j] == tabuleiro[i][j + 3] && tabuleiro[i][j] != 'B') {
                     return true;
+                }
+            }
+        }
+        //Verificar Diagonais
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro.length + 1; j++) {
+                if (j < 4 && i < 3) {
+                    if (tabuleiro[i][j] == tabuleiro[i + 1][j + 1] && tabuleiro[i][j] == tabuleiro[i + 2][j + 2] && tabuleiro[i][j] == tabuleiro[i + 3][j + 3] && tabuleiro[i][j] != 'B') {
+                        return true;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        //Verificar Diagonais Contrárias
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro.length + 1; j = j + 1) {
+                if (j > 2 && i < 3) {
+                    if (tabuleiro[i][j] == tabuleiro[i + 1][j - 1] && tabuleiro[i][j] == tabuleiro[i + 2][j - 2] && tabuleiro[i][j] == tabuleiro[i + 3][j - 3] && tabuleiro[i][j] != 'B') {
+                        return true;
+                    }
+                }
+            }
+        }
+        //Verificar Colunas
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro.length + 1; j = j + 1) {
+                if (i > 2) {
+                    break;
+                } else {
+                    if (tabuleiro[i][j] == tabuleiro[i + 1][j] && tabuleiro[i][j] == tabuleiro[i + 2][j] && tabuleiro[i][j] == tabuleiro[i + 3][j] && tabuleiro[i][j] != 'B') {
+                        return true;
+                    }
                 }
             }
         }
